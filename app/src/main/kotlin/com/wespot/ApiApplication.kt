9b3f3b420f05e26a44fd95fc8b965044a784cb8e
sectionr0 <sectionr0@gmail.com>
2024-06-30 02:39:12 +0900

    feat: app 모듈 추가 및 기본 application yml 설정

---
+++ app/src/main/kotlin/com/wespot/ApiApplication.kt
@@ -0,0 +1,15 @@
+package com.wespot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@EnableFeignClients
+@SpringBootApplication(scanBasePackages = ["come.wespot"])
+class ApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiApplication>(*args)
+}
