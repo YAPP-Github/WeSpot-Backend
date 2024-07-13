5b99d8d87d70e4917a1cc37efc6d0e11531b2bb7
jaeyeon kim <jakind@naver.com>
2024-07-13 22:49:05 +0900

    feat: VoteUseCase 정의

--- core/src/main/kotlin/com/wespot/vote/port/in/VoteUseCase.kt
+++ core/src/main/kotlin/com/wespot/vote/port/in/VoteUseCase.kt
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service
 
 @Service
 interface VoteUseCase {
+
+    fun getVoteOptions(userId: Long, voteOptionId: Long)
+
+    fun saveVote(userId: Long, voteOptionId: Long)
+
 }
