546af0f1bbdf7118d898d9fbd5742b3446719b3a
jaeyeon kim <jakind@naver.com>
2024-07-14 13:47:05 +0900

    feat: Domain에 Validation 추가

--- domain/src/main/kotlin/com/wespot/vote/Ballot.kt
+++ domain/src/main/kotlin/com/wespot/vote/Ballot.kt
@@ -3,13 +3,31 @@ package com.wespot.vote
 import com.wespot.user.User
 import com.wespot.voteoption.VoteOption
 import java.time.LocalDateTime
+import java.util.*
 
 data class Ballot(
-    val id: Long,
+    val id: Long?,
     val voteOption: VoteOption,
     val sender: User,
     val receiver: User,
-    val createdAt: LocalDateTime,
+    val createdAt: LocalDateTime?,
     val isReceiverRead: Boolean,
 ) {
+
+    companion object {
+        fun of(voteOption: VoteOption, sender: User, receiver: User): Ballot {
+            if (sender == receiver) {
+                throw IllegalArgumentException("본인을 투표할 수 없습니다.")
+            }
+            return Ballot(
+                id = null,
+                voteOption = voteOption,
+                sender = sender,
+                receiver = receiver,
+                createdAt = null,
+                isReceiverRead = false
+            )
+        }
+    }
+
 }
--- domain/src/main/kotlin/com/wespot/voteoption/VoteOption.kt
+++ domain/src/main/kotlin/com/wespot/voteoption/VoteOption.kt
@@ -1,11 +1,32 @@
 package com.wespot.voteoption
 
 import java.time.LocalDateTime
+import java.util.*
 
 data class VoteOption(
-    val id: Long,
+    val id: Long?,
     val content: String,
-    val createdAt: LocalDateTime,
-    val updatedAt: LocalDateTime,
+    val createdAt: LocalDateTime?,
+    val updatedAt: LocalDateTime?,
 ) {
+
+    companion object {
+        fun of(
+            id: Long,
+            content: String,
+            createdAt: LocalDateTime,
+            updatedAt: LocalDateTime
+        ): VoteOption {
+            if (Objects.isNull(content) || content.isBlank()) {
+                throw IllegalArgumentException("선택지의 내용은 필수로 존재해야합니다.")
+            }
+            return VoteOption(
+                id = id,
+                content = content,
+                createdAt = createdAt,
+                updatedAt = updatedAt
+            )
+        }
+    }
+
 }
