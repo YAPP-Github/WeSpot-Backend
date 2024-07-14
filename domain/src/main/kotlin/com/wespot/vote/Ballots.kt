9644b5130540785741bdb3620d8a8a4dab1f2e4b
jaeyeon kim <jakind@naver.com>
2024-07-14 13:21:30 +0900

    feat: Ballot의 일급 컬렉션 정의

---
+++ domain/src/main/kotlin/com/wespot/vote/Ballots.kt
@@ -0,0 +1,21 @@
+package com.wespot.vote
+
+import com.wespot.user.User
+
+data class Ballots(
+    private val ballots: MutableList<Ballot>,
+) {
+
+    fun addBallot(ballot: Ballot) {
+        ballots.add(ballot);
+    }
+
+    fun findUserVotedByUser(sentUser: User): List<User> {
+        return ballots.stream()
+            .filter { ballot -> ballot.sender == sentUser }
+            .map(Ballot::receiver)
+            .distinct()
+            .toList()
+    }
+
+}
