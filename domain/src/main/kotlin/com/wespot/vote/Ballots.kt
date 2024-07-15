a3d96eda48aa12651d1478465551f505b62c0cab
jaeyeon kim <jakind@naver.com>
2024-07-15 11:05:52 +0900

    feat: Ballots 예외사항 추가

--- domain/src/main/kotlin/com/wespot/vote/Ballots.kt
+++ domain/src/main/kotlin/com/wespot/vote/Ballots.kt
@@ -13,11 +13,27 @@ data class Ballots(
     }
 
     fun add(ballot: Ballot) {
+        validateNull(ballot)
+        validateDuplicateVote(ballot)
+        ballots.add(ballot)
+    }
+
+    private fun validateNull(ballot: Ballot) {
         if (Objects.isNull(ballot)) {
             throw IllegalArgumentException("빈 투표지를 제출할 수 없습니다.")
         }
-        ballots.add(ballot)
     }
+
+    private fun validateDuplicateVote(ballot: Ballot) {
+        val isDuplicateBallot: Boolean = ballots.stream()
+            .anyMatch { it -> isVoteSameClassmate(it, ballot) }
+        if (isDuplicateBallot) {
+            throw IllegalArgumentException("하루에 한 명의 회원에게 한 개의 투표만 할 수 있습니다.")
+        }
+    }
+
+    private fun isVoteSameClassmate(it: Ballot, ballot: Ballot) =
+        it.senderId == ballot.senderId && it.receiverId == ballot.receiverId
 
     fun findUserIdsVotedByUser(sentUserId: Long): List<Long> {
         return ballots.stream()
