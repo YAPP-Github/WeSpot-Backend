6efb8501f8185bee9eb080700f3d7f7e5e9e51d8
sectionr0 <sectionr0@gmail.com>
2024-07-16 12:27:31 +0900

    refactor: Social 도메인 수정

--- domain/src/main/kotlin/com/wespot/user/Social.kt
+++ domain/src/main/kotlin/com/wespot/user/Social.kt
@@ -2,8 +2,23 @@ package com.wespot.user
 
 data class Social(
     val socialType: SocialType,
-    val socialId: Long,
+    val socialId: String,
     val socialEmail: String?,
-    val socialRefreshToken: String
+    val socialRefreshToken: String?
 ) {
+
+    companion object{
+        fun create(
+            email : String,
+            socialEmail: String,
+            socialRefreshToken: String
+        ): Social {
+            return Social(
+                socialType = SocialType.valueOf(email.split("@")[1]),
+                socialId = email.split("@")[0],
+                socialEmail = socialEmail,
+                socialRefreshToken = socialRefreshToken
+            )
+        }
+    }
 }
