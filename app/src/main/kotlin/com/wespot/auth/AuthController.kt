652bf3fc9d7e7af3550462d0120b6e889f57df27
조기천 <sectionr0@gmail.com>
2024-07-17 15:01:44 +0900

    refactor: api prefix 제거

--- app/src/main/kotlin/com/wespot/auth/AuthController.kt
+++ app/src/main/kotlin/com/wespot/auth/AuthController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/v1/auth")
 class AuthController(
     private val authService: AuthService
 ) {
