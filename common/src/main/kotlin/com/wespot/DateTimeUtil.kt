5b16d8fa7f084fa351c5b737a31d110cd2182316
sectionr0 <sectionr0@gmail.com>
2024-07-19 19:01:15 +0900

    refactor: EOF 추가

--- common/src/main/kotlin/com/wespot/DateTimeUtil.kt
+++ common/src/main/kotlin/com/wespot/DateTimeUtil.kt
@@ -5,9 +5,11 @@ import java.time.LocalDateTime
 import java.time.ZoneId
 
 object DateTimeUtil {
+
     fun getExpirationLocalDateTime(expireTimeInMillis: Long): LocalDateTime {
         val now = Instant.now()
         val expirationInstant = now.plusMillis(expireTimeInMillis)
         return LocalDateTime.ofInstant(expirationInstant, ZoneId.systemDefault())
     }
+
 }
