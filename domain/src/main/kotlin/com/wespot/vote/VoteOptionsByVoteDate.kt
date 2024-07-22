0a206fd65b75b33a1d3a182dd81ea669bdb18650
jaeyeon kim <jakind@naver.com>
2024-07-22 18:11:47 +0900

    test: 오늘의 질문지 테스트 작성

--- domain/src/main/kotlin/com/wespot/vote/VoteOptionsByVoteDate.kt
+++ domain/src/main/kotlin/com/wespot/vote/VoteOptionsByVoteDate.kt
@@ -13,6 +13,9 @@ data class VoteOptionsByVoteDate(
             if (voteOptions.size != 5) {
                 throw IllegalArgumentException("선택지는 5개가 주어져야 합니다.")
             }
+            if (LocalDate.now() < date) {
+                throw IllegalArgumentException("미래의 선택지는 정할 수 없습니다.")
+            }
 
             return VoteOptionsByVoteDate(date, voteOptions)
         }
