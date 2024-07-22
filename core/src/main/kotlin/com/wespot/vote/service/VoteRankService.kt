0d70fa18339d18934a50382600447a6e65985f81
jaeyeon kim <jakind@naver.com>
2024-07-22 16:05:27 +0900

    feat: 순위 반환 서비스 로직 작성

--- core/src/main/kotlin/com/wespot/vote/service/VoteRankService.kt
+++ core/src/main/kotlin/com/wespot/vote/service/VoteRankService.kt
@@ -1,11 +1,14 @@
 package com.wespot.vote.service
 
 import com.wespot.user.port.out.UserPort
+import com.wespot.vote.VoteRecord
 import com.wespot.vote.dto.response.top1.VoteResultResponsesOfTop1
 import com.wespot.vote.dto.response.top5.VoteResultResponsesOfTop5
 import com.wespot.vote.port.`in`.VoteRankUseCase
 import com.wespot.vote.port.out.VoteOptionPort
 import com.wespot.vote.port.out.VotePort
+import com.wespot.vote.service.helper.VoteServiceHelper
+import com.wespot.voteoption.VoteOption
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
@@ -17,11 +20,23 @@ class VoteRankService(
 ) : VoteRankUseCase {
 
     override fun getVoteResultsOfTop5(userId: Long, date: LocalDate): VoteResultResponsesOfTop5 {
-        TODO("Not yet implemented")
+        return VoteResultResponsesOfTop5.from(getRankedVoteResults(userId, date))
+    }
+
+    private fun getRankedVoteResults(
+        userId: Long,
+        date: LocalDate
+    ): Map<VoteOption, List<VoteRecord>> {
+        val user = VoteServiceHelper.findUser(userPort, userId)
+        val classmates = VoteServiceHelper.findClassmatesByUser(userPort, user)
+        val vote = VoteServiceHelper.findVoteByUser(votePort, user, date)
+        val voteOptions = VoteServiceHelper.findVoteOptionsByVoteDate(voteOptionPort, vote)
+        val rankedVoteResults = vote.getRankedVoteResults(voteOptions, classmates)
+        return rankedVoteResults
     }
 
     override fun getVoteResultsOfTop1(userId: Long, date: LocalDate): VoteResultResponsesOfTop1 {
-        TODO("Not yet implemented")
+        return VoteResultResponsesOfTop1.from(getRankedVoteResults(userId, date))
     }
 
 }
