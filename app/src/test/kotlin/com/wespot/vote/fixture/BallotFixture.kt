4322180a14e550a56252a6a5a9411cdd089478da
jaeyeon kim <jakind@naver.com>
2024-07-23 18:13:25 +0900

    test: 투표지 저장 Repository Test 추가

--- app/src/test/kotlin/com/wespot/vote/fixture/BallotFixture.kt
+++ app/src/test/kotlin/com/wespot/vote/fixture/BallotFixture.kt
@@ -6,7 +6,7 @@ import java.time.LocalDateTime
 object BallotFixture {
 
     fun create() = Ballot(
-        id = 1L,
+        id = 0L,
         voteId = 1L,
         voteOptionId = 1L,
         senderId = 1L,
@@ -35,7 +35,7 @@ object BallotFixture {
         voteOptionId: Long,
         senderId: Long,
         receiverId: Long,
-        createdAt:LocalDateTime
+        createdAt: LocalDateTime
     ) = Ballot(
         id = 0L,
         voteId = voteId,
@@ -44,6 +44,23 @@ object BallotFixture {
         receiverId = receiverId,
         createdAt = createdAt,
         isReceiverRead = false
+    )
+
+    fun createByVoteAndVoteOptionAndSenderAndReceiverAndCreatedAtAndIsReceiverRead(
+        voteId: Long,
+        voteOptionId: Long,
+        senderId: Long,
+        receiverId: Long,
+        createdAt: LocalDateTime,
+        isReceiverRead: Boolean
+    ) = Ballot(
+        id = 0L,
+        voteId = voteId,
+        voteOptionId = voteOptionId,
+        senderId = senderId,
+        receiverId = receiverId,
+        createdAt = createdAt,
+        isReceiverRead = isReceiverRead
     )
 
 }
--- app/src/test/kotlin/com/wespot/vote/fixture/BallotJpaEntityFixture.kt
+++ app/src/test/kotlin/com/wespot/vote/fixture/BallotJpaEntityFixture.kt
@@ -7,13 +7,23 @@ import java.time.LocalDateTime
 object BallotJpaEntityFixture {
 
     fun create() = BallotJpaEntity(
-        id = 1L,
+        id = 0L,
         voteId = 1L,
         voteOptionId = 1L,
         senderId = 1L,
         receiverId = 1L,
         baseEntity = BaseEntity(LocalDateTime.now(), null),
-        isReceiverRead = true
+        isReceiverRead = false
+    )
+
+    fun createWithId(id: Long) = BallotJpaEntity(
+        id = id,
+        voteId = 1L,
+        voteOptionId = 1L,
+        senderId = 1L,
+        receiverId = 1L,
+        baseEntity = BaseEntity(LocalDateTime.now(), null),
+        isReceiverRead = false
     )
 
 }
