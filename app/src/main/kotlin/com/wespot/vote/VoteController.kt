ea9646bce07d1694c1ca0b97c6e9e13037501267
jaeyeon kim <jakind@naver.com>
2024-07-23 18:12:49 +0900

    feat: 내가 보낸, 받은 투표 목록, 개별 조회 API 추가

--- app/src/main/kotlin/com/wespot/vote/VoteController.kt
+++ app/src/main/kotlin/com/wespot/vote/VoteController.kt
@@ -3,13 +3,20 @@ package com.wespot.vote
 import com.wespot.vote.dto.request.VoteRequests
 import com.wespot.vote.dto.response.SaveVoteResponse
 import com.wespot.vote.dto.response.VoteItems
+import com.wespot.vote.dto.response.received.ReceivedVoteResponse
+import com.wespot.vote.dto.response.received.ReceivedVotesResponses
+import com.wespot.vote.dto.response.sent.SentVoteResponse
+import com.wespot.vote.dto.response.sent.SentVotesResponses
 import com.wespot.vote.dto.response.top1.VoteResultResponsesOfTop1
 import com.wespot.vote.dto.response.top5.VoteResultResponsesOfTop5
+import com.wespot.vote.port.`in`.ReceivedVoteUseCase
 import com.wespot.vote.port.`in`.SavedVoteUseCase
+import com.wespot.vote.port.`in`.SentVoteUseCase
 import com.wespot.vote.port.`in`.VoteRankUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,7 +28,9 @@ import java.time.LocalDate
 @RequestMapping("/v1/votes")
 class VoteController(
     private val savedVoteUseCase: SavedVoteUseCase,
-    private val voteRankUseCase: VoteRankUseCase
+    private val voteRankUseCase: VoteRankUseCase,
+    private val receivedVoteUseCase: ReceivedVoteUseCase,
+    private val sentVoteUseCase: SentVoteUseCase
 ) {
 
     @GetMapping("/options")
@@ -55,6 +64,41 @@ class VoteController(
         val responses: VoteResultResponsesOfTop1 = voteRankUseCase.getVoteResultsOfTop1(date)
 
         return ResponseEntity.ok(responses)
+    }
+
+    @GetMapping("/received")
+    fun getReceivedVotes(): ResponseEntity<ReceivedVotesResponses> {
+        val responses = receivedVoteUseCase.getReceivedVotes()
+
+        return ResponseEntity.ok(responses)
+    }
+
+    @GetMapping("/received/options/{optionId}")
+    fun getReceivedVote(
+        userId: Long,
+        @PathVariable optionId: Long,
+        @RequestParam date: LocalDate
+    ): ResponseEntity<ReceivedVoteResponse> {
+        val response = receivedVoteUseCase.getReceivedVote(optionId, date)
+
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/sent")
+    fun getSentVotes(): ResponseEntity<SentVotesResponses> {
+        val responses = sentVoteUseCase.getSentVotes()
+
+        return ResponseEntity.ok(responses)
+    }
+
+    @GetMapping("/sent/options/{optionId}")
+    fun getSentVote(
+        @PathVariable optionId: Long,
+        @RequestParam date: LocalDate
+    ): ResponseEntity<SentVoteResponse> {
+        val response = sentVoteUseCase.getSentVote(optionId, date)
+
+        return ResponseEntity.ok(response)
     }
 
 }
