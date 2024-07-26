aa638f5ead4cbb335011f63b9e8bf541254c7ceb
jaeyeon kim <jakind@naver.com>
2024-07-27 03:21:03 +0900

    refactor: 다시 제거

--- app/src/main/kotlin/com/wespot/config/security/SecurityConfig.kt
+++ app/src/main/kotlin/com/wespot/config/security/SecurityConfig.kt
@@ -68,14 +68,6 @@ class SecurityConfig(
                         "/api/v1/users/backgrounds",
                         "/api/v1/users/characters",
                         "/api/v1/users/search",
-                        "/api/v1/votes/options",
-                        "/api/v1/votes",
-                        "/api/v1/votes/tops",
-                        "/api/v1/votes/sent",
-                        "/api/v1/votes/received",
-                        "/api/v1/votes/sent/options",
-                        "/api/v1/votes/received/options",
-                        "/api/v1/reports/users"
                     ).permitAll()
                     .anyRequest().authenticated()
             }
