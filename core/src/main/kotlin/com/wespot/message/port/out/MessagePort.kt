3c53c879fbf873671f9339246ec4ead3fab576bd
sectionr0 <sectionr0@gmail.com>
2024-07-27 12:58:43 +0900

    feat: repository 설정 및 message 쿼리 작성

--- core/src/main/kotlin/com/wespot/message/port/out/MessagePort.kt
+++ core/src/main/kotlin/com/wespot/message/port/out/MessagePort.kt
@@ -1,7 +1,39 @@
 package com.wespot.message.port.out
 
+import com.wespot.message.Message
+import com.wespot.message.MessageType
+import org.springframework.data.domain.Pageable
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 interface MessagePort {
+
+    fun save(message: Message): Message
+
+    fun sendMessageCount(userId: Long): Int
+
+    fun findById(id: Long): Message?
+
+    fun delete()
+
+    fun findAllMessagesByTypeAndReceiverAfterCursor(
+        messageType: MessageType,
+        receiverId: Long,
+        cursorId: Long,
+        pageable: Pageable
+    ): List<Message>
+
+    fun findAllMessagesByTypeAndSenderAfterCursor(
+        messageType: MessageType,
+        senderId: Long,
+        cursorId: Long,
+        pageable: Pageable
+    ): List<Message>
+
+    fun hasSentMessageToday(
+        senderId: Long,
+        receiverId: Long
+    ): Boolean
 
     fun deleteById(id: Long)
 
