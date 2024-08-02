4779af21e4f307134caf6571e13fcbb22a44154a
jaeyeon kim <jakind@naver.com>
2024-08-03 05:37:56 +0900

    feature: Notification 추가

--- domain/src/main/kotlin/com/wespot/notification/Notification.kt
+++ domain/src/main/kotlin/com/wespot/notification/Notification.kt
@@ -1,17 +1,116 @@
 package com.wespot.notification
 
-import com.wespot.user.User
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class Notification(
     val id: Long,
-    val user: User,
+    val userId: Long,
     val type: NotificationType,
+    val date: LocalDate,
     val targetId: Long,
-    val content: String,
-    val isRead: Boolean,
-    val readAt: LocalDateTime,
-    val isEnabled: Boolean,
+    val title: String,
+    val body: String,
+    var isRead: Boolean,
+    var readAt: LocalDateTime,
+    var isEnabled: Boolean,
     val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
 ) {
+
+    companion object {
+
+        fun createVoteInitialState(
+            userId: Long,
+            type: NotificationType,
+            date: LocalDate,
+            title: String,
+            body: String,
+        ): Notification {
+            validateVoteType(type)
+            return Notification(
+                id = 0L,
+                userId = userId,
+                type = type,
+                date = date,
+                targetId = 0,
+                title = title,
+                body = body,
+                isRead = false,
+                readAt = LocalDateTime.now(),
+                isEnabled = true,
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now()
+            )
+        }
+
+        private fun validateVoteType(type: NotificationType) {
+            require(
+                type == NotificationType.VOTE
+                    || type == NotificationType.VOTE_RESULT
+                    || type == NotificationType.VOTE_RECEIVED
+            ) { "투표 관련 알림이 아닙니다." }
+        }
+
+        fun createMessageInitialState(
+            userId: Long,
+            type: NotificationType,
+            targetId: Long,
+            title: String,
+            body: String,
+        ): Notification {
+            validateMessageType(type)
+            return Notification(
+                id = 0L,
+                userId = userId,
+                type = type,
+                date = LocalDate.now(),
+                targetId = targetId,
+                title = title,
+                body = body,
+                isRead = false,
+                readAt = LocalDateTime.now(),
+                isEnabled = true,
+                createdAt = LocalDateTime.now(),
+                updatedAt = LocalDateTime.now()
+            )
+        }
+
+        private fun validateMessageType(type: NotificationType) {
+            require(
+                type == NotificationType.MESSAGE
+                    || type == NotificationType.MESSAGE_SENT
+                    || type == NotificationType.MESSAGE_RECEIVED
+            ) { "쪽지 관련 알림이 아닙니다." }
+        }
+
+    }
+
+    fun read(readerId: Long) {
+        require(readerId == userId) { "알림 수신자만 알림을 조회할 수 있습니다." }
+        this.isRead = true
+        this.readAt = LocalDateTime.now()
+    }
+
+    fun disableVoteNotification(enabledDate: LocalDate) {
+        if (NotificationType.VOTE != type || createdAt.toLocalDate() == enabledDate) {
+            return
+        }
+        isEnabled = false
+    }
+
+    fun disableMessageNotification() {
+        if (!isMessageHomeNotification()) return
+        isEnabled = false
+    }
+
+    fun enableMessageNotification() {
+        if (!isMessageHomeNotification()) return
+        isEnabled = true
+    }
+
+    private fun isMessageHomeNotification(): Boolean {
+        return NotificationType.MESSAGE == type
+    }
+
 }
