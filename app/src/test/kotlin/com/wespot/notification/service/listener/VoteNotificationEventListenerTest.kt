5acfb6f167361ba91352715c044ee33a5abd5350
jaeyeon kim <jakind@naver.com>
2024-08-03 05:50:11 +0900

    feat: VoteNotificationEventListener 추가

---
+++ app/src/test/kotlin/com/wespot/notification/service/listener/VoteNotificationEventListenerTest.kt
@@ -0,0 +1,128 @@
+package com.wespot.notification.service.listener
+
+import com.wespot.DatabaseCleanup
+import com.wespot.notification.NotificationType
+import com.wespot.notification.port.out.NotificationPort
+import com.wespot.notification.service.NotificationSendService
+import com.wespot.user.event.SignUpUserEvent
+import com.wespot.user.fixture.UserFixture
+import com.wespot.user.port.out.UserPort
+import com.wespot.vote.Vote
+import com.wespot.vote.VoteIdentifier
+import com.wespot.vote.event.EndVoteEvent
+import com.wespot.vote.event.ReceivedVoteEvent
+import com.wespot.vote.event.RegisteredVoteEvent
+import com.wespot.vote.port.out.VoteOptionPort
+import com.wespot.voteoption.fixture.VoteOptionFixture
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.AfterEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+import java.time.LocalDateTime
+import kotlin.test.Test
+
+@SpringBootTest
+class VoteNotificationEventListenerTest @Autowired constructor(
+    private val voteNotificationEventListener: VoteNotificationEventListener,
+    private val userPort: UserPort,
+    private val voteOptionPort: VoteOptionPort,
+    private val notificationPort: NotificationPort,
+    private val databaseCleanup: DatabaseCleanup
+) {
+
+    @AfterEach
+    fun tearDown() {
+        databaseCleanup.execute()
+    }
+
+    @Test
+    fun `학급에 새로운 친구가 가입하면 알림이 발송된다`() {
+        // given
+        val user1 = userPort.save(UserFixture.createWithId(0))
+        val user2 = userPort.save(UserFixture.createWithId(0))
+        val sendService = mockk<NotificationSendService>()
+
+        // when
+        every { sendService.sendNotification(any(), any()) } returns Unit
+        voteNotificationEventListener.signUpNewUser(SignUpUserEvent(user2))
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 1
+        notifications[0].userId shouldBe user1.id
+        notifications[0].type shouldBe NotificationType.VOTE
+        notifications[0].date shouldBe LocalDate.now()
+    }
+
+    @Test
+    fun `새로운 인원이 투표에 참여한 경우 알림이 발송된다`() {
+        // given
+        val sendService = mockk<NotificationSendService>()
+        val users = (1..6).map { userPort.save(UserFixture.createWithIdAndEmail(0, "hello${it}@Kakao")) }
+        val voteOptions = (1..5).map { voteOptionPort.save(VoteOptionFixture.create()) }
+        val voteIdentifier = VoteIdentifier.of(users[0], LocalDate.now())
+        val vote = Vote.of(voteIdentifier, voteOptions, null)
+        (0..4).forEach { vote.addBallot(voteOptions[0].id, users[it], users[it + 1], LocalDateTime.now()) }
+
+        // when
+        every { sendService.sendMulticastNotification(any(), any()) } returns Unit
+        voteNotificationEventListener.registerVote(RegisteredVoteEvent(users[0], vote))
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 5
+        notifications[0].userId shouldBe users[1].id
+        notifications[1].userId shouldBe users[2].id
+        notifications[2].userId shouldBe users[3].id
+        notifications[3].userId shouldBe users[4].id
+        notifications[0].type shouldBe NotificationType.VOTE_RESULT
+        notifications[0].targetId shouldBe 0
+        notifications[0].date shouldBe LocalDate.now()
+    }
+
+    @Test
+    fun `투표를 받은 이에게 알림이 발송된다`() {
+        // given
+        val receiver = userPort.save(UserFixture.createWithId(0))
+        val sendService = mockk<NotificationSendService>()
+
+        // when
+        every { sendService.sendNotification(any(), any()) } returns Unit
+        voteNotificationEventListener.receiveVote(ReceivedVoteEvent(receiver))
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 1
+        notifications[0].userId shouldBe receiver.id
+        notifications[0].type shouldBe NotificationType.VOTE_RECEIVED
+        notifications[0].targetId shouldBe 0
+        notifications[0].date shouldBe LocalDate.now()
+    }
+
+    @Test
+    fun `투표가 종료되었을 때, 반 친구들에게 알림이 발송된다`() {
+        // given
+        val sendService = mockk<NotificationSendService>()
+        val users = (1..5).map { userPort.save(UserFixture.createWithIdAndEmail(0, "hello${it}@Kakao")) }
+
+        // when
+        every { sendService.sendNotification(any(), any()) } returns Unit
+        voteNotificationEventListener.endVote(EndVoteEvent())
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 5
+        notifications[0].userId shouldBe users[0].id
+        notifications[1].userId shouldBe users[1].id
+        notifications[2].userId shouldBe users[2].id
+        notifications[3].userId shouldBe users[3].id
+        notifications[4].userId shouldBe users[4].id
+        notifications[0].type shouldBe NotificationType.VOTE_RESULT
+        notifications[0].targetId shouldBe 0
+        notifications[0].date shouldBe LocalDate.now().minusDays(1)
+    }
+
+}
