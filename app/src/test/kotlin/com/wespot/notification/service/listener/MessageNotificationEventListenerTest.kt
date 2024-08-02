a8a697eee12d5144d2fe41b687c785c93980981d
jaeyeon kim <jakind@naver.com>
2024-08-03 05:51:08 +0900

    feat: MessageNotificationEventListener 추가

---
+++ app/src/test/kotlin/com/wespot/notification/service/listener/MessageNotificationEventListenerTest.kt
@@ -0,0 +1,106 @@
+package com.wespot.notification.service.listener
+
+import com.wespot.DatabaseCleanup
+import com.wespot.message.event.MessageLimitEvent
+import com.wespot.message.event.ReadMessageByReceiverEvent
+import com.wespot.message.event.ReceivedMessageEvent
+import com.wespot.message.fixture.MessageFixture
+import com.wespot.message.port.out.MessagePort
+import com.wespot.notification.NotificationType
+import com.wespot.notification.fixtrue.NotificationFixture
+import com.wespot.notification.port.out.NotificationPort
+import com.wespot.notification.service.NotificationSendService
+import com.wespot.user.fixture.UserFixture
+import com.wespot.user.port.out.UserPort
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageNotificationEventListenerTest @Autowired constructor(
+    private val messageNotificationEventListener: MessageNotificationEventListener,
+    private val databaseCleanup: DatabaseCleanup,
+    private val userPort: UserPort,
+    private val messagePort: MessagePort,
+    private val notificationPort: NotificationPort,
+) {
+
+    @AfterEach
+    fun tearDown() {
+        databaseCleanup.execute()
+    }
+
+    @Test
+    fun `쪽지를 3개를 보내 알림이 비활성화된다`() {
+        // given
+        val sender = userPort.save(UserFixture.createWithId(0))
+        val receiver = userPort.save(UserFixture.createWithId(0))
+        val message = messagePort.save(MessageFixture.createWithIdAndSenderIdAndReceiverId(0, sender.id, receiver.id))
+        val notification = notificationPort.save(
+            NotificationFixture.createWithIdAndUserIdAndTypeAndTargetId(
+                0,
+                sender.id,
+                NotificationType.MESSAGE,
+                message.id
+            )
+        )
+
+        // when
+        messageNotificationEventListener.disableMessageNotificationByLimit(MessageLimitEvent(message.id, 3))
+        val disableNotification = notificationPort.findById(notification.id)
+
+        // then
+        disableNotification!!.isEnabled shouldBe false
+    }
+
+    @Test
+    fun `쪽지를 받은 이에게 알림이 발송된다`() {
+        // given
+        val sender = userPort.save(UserFixture.createWithId(0))
+        val receiver = userPort.save(UserFixture.createWithId(0))
+        val message = messagePort.save(MessageFixture.createWithIdAndSenderIdAndReceiverId(0, sender.id, receiver.id))
+        val sendService = mockk<NotificationSendService>()
+
+        // when
+        every { sendService.sendNotification(any(), any()) } returns Unit
+        messageNotificationEventListener.receiveMessage(ReceivedMessageEvent(receiver, message.id))
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 1
+        notifications[0].userId shouldBe receiver.id
+        notifications[0].type shouldBe NotificationType.MESSAGE_RECEIVED
+    }
+
+    @Test
+    fun `수신자가 쪽지를 읽으면 송신자에게 알림이 발송된다`() {
+        // given
+        val sender = userPort.save(UserFixture.createWithId(0))
+        val receiver = userPort.save(UserFixture.createWithId(0))
+        val message = messagePort.save(MessageFixture.createWithIdAndSenderIdAndReceiverId(0, sender.id, receiver.id))
+        val sendService = mockk<NotificationSendService>()
+
+        // when
+        every { sendService.sendNotification(any(), any()) } returns Unit
+        messageNotificationEventListener.readMessageByReceiver(
+            ReadMessageByReceiverEvent(
+                sender,
+                receiver,
+                message.id,
+                false
+            )
+        )
+        val notifications = notificationPort.findAll()
+
+        // then
+        notifications.size shouldBe 1
+        notifications[0].userId shouldBe sender.id
+        notifications[0].type shouldBe NotificationType.MESSAGE_SENT
+        notifications[0].targetId shouldBe message.id
+    }
+
+}
