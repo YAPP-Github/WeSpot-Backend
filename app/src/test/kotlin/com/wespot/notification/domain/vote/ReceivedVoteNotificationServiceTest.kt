5a73b75e5fc53625aebc54838c62e379519106e7
jaeyeon kim <jakind@naver.com>
2024-08-03 05:53:45 +0900

    test: ReceivedVoteNotificationService Test 추가

---
+++ app/src/test/kotlin/com/wespot/notification/domain/vote/ReceivedVoteNotificationServiceTest.kt
@@ -0,0 +1,39 @@
+package com.wespot.notification.domain.vote
+
+import com.wespot.notification.NotificationType
+import com.wespot.notification.vote.ReceivedVoteNotificationService
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import java.time.LocalDate
+
+class ReceivedVoteNotificationServiceTest : BehaviorSpec({
+
+    given("남학생이 투표를 완료했을 때") {
+        val service = ReceivedVoteNotificationService()
+        `when`("투표를 받은이에게") {
+            val notification = service.getNotification(100L, "female")
+            then("알림이 발송된다.") {
+                notification.userId shouldBe 100L
+                notification.type shouldBe NotificationType.VOTE_RECEIVED
+                notification.targetId shouldBe 0
+                notification.date shouldBe LocalDate.now()
+                notification.title shouldBe "방금 우리 반 여학생이 나에게 투표했어요 \uD83D\uDCA5"
+            }
+        }
+    }
+
+    given("여학생이 투표를 완료했을 때") {
+        val service = ReceivedVoteNotificationService()
+        `when`("투표를 받은이에게") {
+            val notification = service.getNotification(100L, "male")
+            then("알림이 발송된다.") {
+                notification.userId shouldBe 100L
+                notification.type shouldBe NotificationType.VOTE_RECEIVED
+                notification.targetId shouldBe 0
+                notification.date shouldBe LocalDate.now()
+                notification.title shouldBe "방금 우리 반 남학생이 나에게 투표했어요 \uD83D\uDCA5"
+            }
+        }
+    }
+
+})
