5bc2e577499681b354aa182115c3d51ea5683a5c
jaeyeon kim <jakind@naver.com>
2024-08-05 22:23:35 +0900

    refactor: 실제 필터링 추가

--- core/src/main/kotlin/com/wespot/notification/service/NotificationHelper.kt
+++ core/src/main/kotlin/com/wespot/notification/service/NotificationHelper.kt
@@ -1,29 +1,35 @@
 package com.wespot.notification.service
 
 import com.wespot.notification.Notification
+import com.wespot.notification.NotificationFilter
 import com.wespot.notification.NotificationInfo
+import com.wespot.notification.NotificationType
 import com.wespot.notification.port.out.NotificationServicePort
 import com.wespot.user.User
 import org.springframework.stereotype.Component
 
 @Component
 class NotificationHelper(
-    private val notificationServicePort: NotificationServicePort
+    private val notificationServicePort: NotificationServicePort,
+    private val notificationFilter: NotificationFilter
 ) {
 
-    fun sendNotifications(users: List<User>, notifications: List<Notification>) {
+    fun sendNotifications(users: List<User>, notifications: List<Notification>, notificationType: NotificationType) {
         if (notifications.isEmpty()) {
             return
         }
 
-        val notificationUserIdGroup = notifications.map { it.userId }.toHashSet()
+        val filteredNotifications = notificationFilter.filterNotifications(users, notifications, notificationType)
+        val notificationUserIdGroup = filteredNotifications.map { it.userId }.toHashSet()
         val notificationUsers = users.filter { notificationUserIdGroup.contains(it.id) }
-        val notificationInfo = NotificationInfo.createInitialState(notifications[0])
+        val notificationInfo = NotificationInfo.createInitialState(filteredNotifications[0])
         notificationServicePort.sendMulticastNotification(notificationUsers, notificationInfo)
     }
 
     fun sendNotification(user: User?, notification: Notification) {
         user ?: return
+        notificationFilter.filterNotification(user, notification, NotificationType.MESSAGE) ?: return
+
         notificationServicePort.sendNotification(
             user,
             NotificationInfo.createInitialState(notification)
