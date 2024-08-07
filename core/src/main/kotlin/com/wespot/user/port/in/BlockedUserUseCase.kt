6a520e9ada520ca2782d1257a680f2214ff31a5b
sectionr0 <sectionr0@gmail.com>
2024-08-07 22:32:24 +0900

    refactor: 차단 api 분리

--- core/src/main/kotlin/com/wespot/user/port/in/BlockedUserUseCase.kt
+++ core/src/main/kotlin/com/wespot/user/port/in/BlockedUserUseCase.kt
@@ -1,7 +1,11 @@
 package com.wespot.user.port.`in`
 
+import com.wespot.user.dto.response.BlockedUserResponse
+
 interface BlockedUserUseCase {
 
-    fun blockedUser(messageId: Long): Boolean
+    fun blockedUser(messageId: Long): BlockedUserResponse
+
+    fun unblockedUser(messageId: Long): Unit
 
 }
--- core/src/main/kotlin/com/wespot/user/service/BlockedUserService.kt
+++ core/src/main/kotlin/com/wespot/user/service/BlockedUserService.kt
@@ -4,7 +4,9 @@ import com.wespot.auth.service.SecurityUtils.getLoginUser
 import com.wespot.message.port.out.MessagePort
 import com.wespot.message.service.MessageFinder.findMessageById
 import com.wespot.message.service.MessageFinder.findUserById
+import com.wespot.user.User
 import com.wespot.user.block.BlockedUser
+import com.wespot.user.dto.response.BlockedUserResponse
 import com.wespot.user.port.`in`.BlockedUserUseCase
 import com.wespot.user.port.out.BlockedUserPort
 import com.wespot.user.port.out.UserPort
@@ -19,54 +21,39 @@ class BlockedUserService(
     private val userPort: UserPort,
 ): BlockedUserUseCase {
 
-    override fun blockedUser(messageId: Long): Boolean {
+    override fun blockedUser(messageId: Long): BlockedUserResponse {
+        val (loginUser, blockedUser) = findAndValidateUsers(messageId)
+        val isAlreadyBlocked = blockedUserPort.existsByBlockerIdAndBlockedIdAndMessageId(
+            blockerId = loginUser.id,
+            blockedId = blockedUser.id,
+            messageId = messageId
+        )
+        check(!isAlreadyBlocked) { "이미 차단된 사용자입니다." }
+
+        val createBlockedUser = BlockedUser.create(
+            blockerId = loginUser.id,
+            blockedId = blockedUser.id,
+            messageId = messageId
+        )
+        val saveBlockedUser = blockedUserPort.save(createBlockedUser)
+        return BlockedUserResponse.of(saveBlockedUser.id)
+    }
+
+    override fun unblockedUser(messageId: Long) {
+        val (loginUser, blockedUser) = findAndValidateUsers(messageId)
+        blockedUserPort.deleteByBlockerIdAndBlockedIdAndMessageId(
+            blockerId = loginUser.id,
+            blockedId = blockedUser.id,
+            messageId = messageId
+        )
+    }
+
+    private fun findAndValidateUsers(messageId: Long): Pair<User, User> {
         val loginUser = getLoginUser(userPort = userPort)
         val message = findMessageById(id = messageId, messagePort = messagePort)
         message.validateReceivedMessage(loginUser)
         val blockedUser = findUserById(id = message.senderId, userPort = userPort)
-
-        return when (isBlocked(loginUser.id, blockedUser.id, messageId)) {
-            true -> unblockUser(loginUser.id, blockedUser.id, messageId)
-            false -> blockUser(loginUser.id, blockedUser.id, messageId)
-        }
+        return Pair(loginUser, blockedUser)
     }
 
-    private fun isBlocked(
-        blockerId: Long,
-        blockedId: Long,
-        messageId: Long
-    ): Boolean {
-        return blockedUserPort.existsByBlockerIdAndBlockedIdAndMessageId(
-            blockerId = blockerId,
-            blockedId = blockedId,
-            messageId = messageId
-        )
-    }
-
-    private fun blockUser(
-        blockerId: Long,
-        blockedId: Long,
-        messageId: Long
-    ): Boolean {
-        val blockedUser = BlockedUser.create(
-            blockerId = blockerId,
-            blockedId = blockedId,
-            messageId = messageId
-        )
-        blockedUserPort.save(blockedUser)
-        return true
-    }
-
-    private fun unblockUser(
-        blockerId: Long,
-        blockedId: Long,
-        messageId: Long
-    ): Boolean {
-        blockedUserPort.deleteByBlockerIdAndBlockedIdAndMessageId(
-            blockerId = blockerId,
-            blockedId = blockedId,
-            messageId = messageId
-        )
-        return false
-    }
 }
