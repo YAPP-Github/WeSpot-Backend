462bfbd2d68ddbb1eeae7f203ceff1505ed45e27
jaeyeon kim <jakind@naver.com>
2024-08-14 02:48:43 +0900

    chore: TimeZone 설정 추가

---
+++ app/src/main/kotlin/com/wespot/config/TimeZoneConfig.kt
@@ -0,0 +1,15 @@
+package com.wespot.config
+
+import jakarta.annotation.PostConstruct
+import org.springframework.context.annotation.Configuration
+import java.util.*
+
+@Configuration
+class TimeZoneConfig {
+
+    @PostConstruct
+    fun timeZoneSetUp() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+
+}
