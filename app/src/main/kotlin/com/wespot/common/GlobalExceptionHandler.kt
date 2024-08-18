a4953a2ad29af50b850fc0fb4e2d527bc97d54ff
sectionr0 <sectionr0@gmail.com>
2024-08-18 12:36:21 +0900

    refactor: 예외처리 추가 및 에러 알림 내용 변경

--- app/src/main/kotlin/com/wespot/common/GlobalExceptionHandler.kt
+++ app/src/main/kotlin/com/wespot/common/GlobalExceptionHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import java.net.URI
@@ -33,6 +34,46 @@ class GlobalExceptionHandler(
             exception.message
         ).apply {
             type = URI.create("/errors/illegal-argument")
+            instance = URI.create(request.requestURI)
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(problemDetail)
+    }
+
+    @ExceptionHandler(IllegalStateException::class)
+    fun handleIllegalStateException(
+        exception: IllegalStateException,
+        request: HttpServletRequest
+    ): ResponseEntity<ProblemDetail> {
+        notifyException(false, request, exception)
+        logger.error("잘못된 상태입니다", exception)
+
+        val problemDetail = ProblemDetail.forStatusAndDetail(
+            HttpStatus.CONFLICT,
+            exception.message
+        ).apply {
+            type = URI.create("/errors/conflict")
+            instance = URI.create(request.requestURI)
+        }
+
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(problemDetail)
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleHttpRequestMethodNotSupported(
+        exception: HttpRequestMethodNotSupportedException,
+        request: HttpServletRequest
+    ): ResponseEntity<ProblemDetail> {
+        notifyException(false, request, exception)
+        logger.error("지원하지 않는 HTTP 메소드입니다.", exception)
+
+        val problemDetail = ProblemDetail.forStatusAndDetail(
+            HttpStatus.BAD_REQUEST,
+            exception.message
+        ).apply {
+            type = URI.create("/errors/method-not-supported")
             instance = URI.create(request.requestURI)
         }
 
@@ -85,20 +126,49 @@ class GlobalExceptionHandler(
             return
         }
 
+        val headers = request.headerNames.toList().joinToString("\n") { headerName ->
+            "$headerName: ${request.getHeader(headerName)}"
+        }
+
+        val parameters = request.parameterNames.toList().joinToString("\n") { paramName ->
+            "$paramName: ${request.getParameter(paramName)}"
+        }
+
+        val fullStackTrace = exception.stackTraceToString().take(3000)
+
+        // 예외 발생 알림을 디스코드에 전송
         errorNotificationUseCase.notifyError(
             isError,
             "### 🕖 발생 시간\n" +
                 "${LocalDateTime.now()}\n" +
-                "### \uD83D\uDD17 요청 URI\n" +
+                "### 📎 요청 URI\n" +
                 "${request.requestURI} (${request.method})\n" +
-                "### ✅ Exception Source\n" +
-                "$exception at ${extractExceptionSource(exception)}\n" +
-                "### \uD83D\uDCC4 Stack Trace\n" +
+                "### 🛠 요청자 정보\n" +
+                "- IP: ${request.remoteAddr}\n" +
+                "- 사용자: ${request.remoteUser ?: "Unknown"}\n" +
+                "- 헤더:\n" +
                 "```\n" +
-                "${exception.stackTraceToString().substring(0, 3000)}\n" +
-                "```"
+                "$headers\n" +
+                "```\n" +
+                "- 요청 파라미터:\n" +
+                "```\n" +
+                "$parameters\n" +
+                "```\n" +
+                "### ✅ 예외 정보\n" +
+                "- 예외 클래스: ${exception.javaClass.canonicalName}\n" +
+                "- 예외 메시지: ${exception.message ?: "No message"}\n" +
+                "- 발생 위치: ${extractExceptionSource(exception)}\n"
+        )
+
+        errorNotificationUseCase.notifyError(
+            isError,
+            """
+            ### 🗂 스택 트레이스 (부분)
+            ```            $fullStackTrace
+            ```        """.trimIndent()
         )
     }
+
 
     private fun extractExceptionSource(exception: Exception): String {
         val stackTrace = exception.stackTrace
