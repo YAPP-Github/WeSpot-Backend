42129142dc040043962b0e708fa572f8d043a2f9
kpeel5839 <89840550+kpeel5839@users.noreply.github.com>
2024-08-24 21:48:22 +0900

    refactor: GlobalExceptionHandler 수정 (#103)

--- common/src/main/kotlin/com/wespot/exception/ExceptionResponse.kt
+++ common/src/main/kotlin/com/wespot/exception/ExceptionResponse.kt
@@ -4,6 +4,24 @@ import org.springframework.http.ProblemDetail
 
 class ExceptionResponse(
     val view: ExceptionView,
-    val problemDetail: ProblemDetail
+    val type: String,
+    val title: String,
+    val status: Int,
+    val detail: String,
+    val instance: String,
 ) {
+    companion object {
+
+        fun of(view: ExceptionView, problemDetail: ProblemDetail): ExceptionResponse {
+            return ExceptionResponse(
+                view = view,
+                type = problemDetail.type.path,
+                title = problemDetail.title!!,
+                status = problemDetail.status,
+                detail = problemDetail.detail!!,
+                instance = problemDetail.instance!!.path,
+            )
+        }
+
+    }
 }
