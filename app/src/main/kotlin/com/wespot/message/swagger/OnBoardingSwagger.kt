09a3eba2af575611f20af1ce11d02177251b4d8a
kpeel <jaeyeon.kim@seavantage.com>
2025-02-28 21:56:16 +0900

    refactor: Swagger 변경

--- app/src/main/kotlin/com/wespot/message/swagger/OnBoardingSwagger.kt
+++ app/src/main/kotlin/com/wespot/message/swagger/OnBoardingSwagger.kt
@@ -12,7 +12,7 @@ interface OnBoardingSwagger {
     @Operation(summary = "온보딩 시트를 받는 서버드리븐 API 입니다.")
     fun getOnBoardingComponents(category: OnBoardingComponentRequest): ResponseEntity<OnBoardingResponse>
 
-    @Operation(summary = "유저가 온보딩 시트를 이미 받아본적이 있는지 여부를 확인하는 API 입니다.")
+    @Operation(summary = "유저가 쪽지 혹은 투표 온보딩 시트를 조회함을 저장하는 API 입니다.")
     fun viewedOnBoardingSheetBy(category: OnBoardingComponentRequest): ResponseEntity<Unit>
 
 }
