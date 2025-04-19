88df0a05067e2097d589fd9da52eea2f0ed40f0d
kpeel <jaeyeon.kim@seavantage.com>
2025-04-19 21:29:40 +0900

    feat: Message Notification 비동기로 변경

--- core/src/main/kotlin/com/wespot/notification/service/listener/MessageNotificationEventListener.kt
+++ core/src/main/kotlin/com/wespot/notification/service/listener/MessageNotificationEventListener.kt
@@ -5,8 +5,12 @@ import com.wespot.message.event.ReadMessageByReceiverEvent
 import com.wespot.message.event.ReceivedMessageEvent
 import com.wespot.notification.port.`in`.MessageNotificationUseCase
 import com.wespot.notification.service.DisabledNotificationService
-import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class MessageNotificationEventListener(
@@ -14,7 +18,9 @@ class MessageNotificationEventListener(
     private val messageNotificationService: MessageNotificationUseCase,
 ) {
 
-    @EventListener
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     fun disableMessageNotificationByLimit(messageLimitEvent: MessageLimitEvent) {
         disabledNotificationService.disableMessageNotification(
             messageLimitEvent.senderId,
@@ -22,12 +28,16 @@ class MessageNotificationEventListener(
         )
     }
 
-    @EventListener
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     fun receiveMessage(receivedMessageEvent: ReceivedMessageEvent) {
         messageNotificationService.receiveMessage(receivedMessageEvent.receiver, receivedMessageEvent.messageId)
     }
 
-    @EventListener
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     fun readMessageByReceiver(readMessageByReceiverEvent: ReadMessageByReceiverEvent) {
         messageNotificationService.readMessageByReceiver(
             readMessageByReceiverEvent.sender,
