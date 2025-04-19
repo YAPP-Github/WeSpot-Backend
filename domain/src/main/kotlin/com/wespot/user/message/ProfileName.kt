c847edb648b9673e4290cd2f2ad763a8640cc5ba
kpeel <jaeyeon.kim@seavantage.com>
2025-04-19 21:50:01 +0900

    feat: 익명 프로필 3개를 초과하여 만들지 못하도록 하고, 익명 프로필 중 같은 이름을 가진 유저도 없도록 함

--- domain/src/main/kotlin/com/wespot/user/message/ProfileName.kt
+++ domain/src/main/kotlin/com/wespot/user/message/ProfileName.kt
@@ -3,6 +3,7 @@ package com.wespot.user.message
 import com.wespot.common.ProfanityChecker
 import com.wespot.exception.CustomException
 import com.wespot.exception.ExceptionView
+import com.wespot.user.User
 import io.micrometer.common.util.StringUtils
 import org.springframework.http.HttpStatus
 
@@ -11,7 +12,11 @@ class ProfileName(
 ) {
 
     companion object {
-        fun of(name: String, names: Set<String>): ProfileName {
+        fun of(name: String, users: List<User>, alreadyCreatedAnonymousProfile: List<AnonymousProfile>): ProfileName {
+            val names = users
+                .map { it.name }
+                .toSet()
+
             if (StringUtils.isBlank(name)) {
                 throw CustomException(HttpStatus.BAD_REQUEST, ExceptionView.TOAST, "익명 프로필로 설정할 이름은 존재해야합니다.")
             }
@@ -22,6 +27,10 @@ class ProfileName(
 
             if (names.contains(name)) {
                 throw CustomException(HttpStatus.BAD_REQUEST, ExceptionView.TOAST, "실명은 익명 프로필로 활용할 수 없습니다.")
+            }
+
+            if (alreadyCreatedAnonymousProfile.any { existsAnonymousProfile -> existsAnonymousProfile.name == name }) {
+                throw CustomException(HttpStatus.BAD_REQUEST, ExceptionView.TOAST, "이미 해당 이름과 동일한 익명 프로필이 존재합니다.")
             }
 
             return ProfileName(name)
