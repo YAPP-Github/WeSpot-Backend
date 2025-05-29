1ba2c791f074e30d0ef150c34b90550030834613
kpeel <jaeyeon.kim@seavantage.com>
2025-05-29 23:33:51 +0900

    refactor: 변경된 쪽지 스펙에 맞게 수정

--- core/src/main/kotlin/com/wespot/message/dto/response/MessageV2OverviewResponse.kt
+++ core/src/main/kotlin/com/wespot/message/dto/response/MessageV2OverviewResponse.kt
@@ -5,34 +5,75 @@ import java.time.LocalDateTime
 
 data class MessageV2OverviewResponse( // TODO : 문서 변경
     val id: Long,
+
+    val senderProfile: MessageProfileResponse,
+
     val isMeMessageRoomOwner: Boolean,
-    val thumbnail: String,
     val isExistsUnreadMessage: Boolean,
     val latestChatTime: LocalDateTime,
     val isAnonymous: Boolean,
-    val name: String,
-    val schoolName: String?,
-    val grade: Int?,
-    val classNumber: Int?,
+
+    val receiverProfile: MessageProfileResponse,
+
     val isBookmarked: Boolean,
     val isBlocked: Boolean,
     val isEver: Boolean
 ) {
+
+    data class MessageProfileResponse(
+        val iconUrl: String,
+        val name: String,
+        val schoolName: String?,
+        val grade: Int?,
+        val classNumber: Int?
+    ) {
+
+        companion object {
+            fun of(
+                iconUrl: String,
+                name: String,
+                schoolName: String?,
+                grade: Int?,
+                classNumber: Int?
+            ): MessageProfileResponse {
+                return MessageProfileResponse(
+                    iconUrl = iconUrl,
+                    name = name,
+                    schoolName = schoolName,
+                    grade = grade,
+                    classNumber = classNumber
+                )
+            }
+        }
+
+    }
 
     companion object {
 
         fun from(room: MessageRoom): MessageV2OverviewResponse {
             return MessageV2OverviewResponse(
                 id = room.id(),
+
+                senderProfile = MessageProfileResponse.of(
+                    iconUrl = room.senderProfileImage(),
+                    name = room.senderName(),
+                    schoolName = room.senderSchoolName(),
+                    grade = room.senderGrade(),
+                    classNumber = room.senderClassNumber()
+                ),
+
                 isMeMessageRoomOwner = room.isViewerOwnerOfMessageRoom(),
-                thumbnail = room.receiverProfileImage(),
                 isExistsUnreadMessage = room.isExistsUnReadMessage(),
                 latestChatTime = room.latestChatTime(),
                 isAnonymous = room.isReceiverUsingAnonymous(),
-                name = room.receiverName(),
-                schoolName = room.receiverSchoolName(),
-                grade = room.receiverGrade(),
-                classNumber = room.receiverClassNumber(),
+                receiverProfile = MessageProfileResponse.of(
+                    iconUrl = room.receiverProfileImage(),
+                    name = room.receiverName(),
+                    schoolName = room.receiverSchoolName(),
+                    grade = room.receiverGrade(),
+                    classNumber = room.receiverClassNumber()
+                ),
+
                 isBookmarked = room.isBookmarked(),
                 isBlocked = room.isBlocked(),
                 isEver = room.isReceiverEver()
