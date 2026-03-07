a79b68fdc7cb58f435615d01bd84868386266c01
kpeel5839 <jakind@naver.com>
2026-03-07 15:33:29 +0900

    chore: 사용하지 않는 import 삭제

--- app/src/main/kotlin/com/wespot/admin/swagger/AdminSwagger.kt
+++ app/src/main/kotlin/com/wespot/admin/swagger/AdminSwagger.kt
@@ -6,9 +6,7 @@ import com.wespot.notification.dto.PublishNotificationTypeResponse
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Admin API", description = "관리자용 API입니다.")
 interface AdminSwagger {
